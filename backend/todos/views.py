d81a5b7b51e497dc996dcaa56a37b55ea8863df4
Barun <bhattacharjeebarun25@gmail.com>
2020-10-15 21:10:02 +0600

    added template in todos app

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,12 +1,19 @@
 from rest_framework import generics
 
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+
 from .models import Todo
 from .serializers import TodoSerializer
 
 class ListTodo(generics.ListAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'todos/listview.html'
 
+    def get(self, request):
+
+        queryset = Todo.objects.all()
+        return Response({'todos': queryset})
 
 class DetailTodo(generics.RetrieveAPIView):
     queryset = Todo.objects.all()
