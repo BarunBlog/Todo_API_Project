40119d8d41a556e2c8373f337c2256acab4f2303
Barun <bhattacharjeebarun25@gmail.com>
2021-02-24 09:28:37 +0600

    Added front end react.js

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,19 +1,11 @@
 from rest_framework import generics
 
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
-
 from .models import Todo
 from .serializers import TodoSerializer
 
 class ListTodo(generics.ListAPIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'todos/listview.html'
-
-    def get(self, request):
-
-        queryset = Todo.objects.all()
-        return Response({'todos': queryset})
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
 class DetailTodo(generics.RetrieveAPIView):
     queryset = Todo.objects.all()
